8710a3658edfa37fda4fd2f288d98cc69347ab98
Ronit gujjar <92150685+Ronit-gurjar@users.noreply.github.com>
2023-06-13 10:04:43 +0000

    adding responisivity.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,28 @@ import { Component } from "react";
 import "./NavbarStyle.css"
 import { MenuItems } from "./MenuItems";
 import { Link } from "react-router-dom";
+import logo from "/workspaces/codespaces-react/src/assets/logo-white.png"
 
 class Navbar extends Component{
+    state= {clicked : false};
+    handleClick = () =>{
+        this.setState({ clicked: !this.state.clicked})
+    }
     render(){
         return(
             <nav className="NavbarItems">
-                <img className="tedx-logo" src="../public/logo-white.png" alt="logo"/>
-                <ul className="navbar-menu">
-                {MenuItems.map((item,index) =>{
-                    return(<li key={index}>
-                        <a className={item.cName} href={item.url}>{item.title}</a>
-                    </li>);
-                })}
+                <img className="tedx-logo" src={logo} alt="logo" />
+
+                <div className="menu-icons" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}>
+                    </i>
+                </div>
+            <ul className={this.state.clicked? "nav-menu active":"nav-menu"}>
+                    {MenuItems.map((item, index) => {
+                        return (<li key={index}>
+                            <a className={item.cName} href={item.url}>{item.title}</a>
+                        </li>);
+                    })}
                 </ul>
             </nav>
         )
