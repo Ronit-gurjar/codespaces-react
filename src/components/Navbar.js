97105cc359a1df76b2b472ec09a7b48aeeb139e7
Ronit gujjar <92150685+Ronit-gurjar@users.noreply.github.com>
2023-06-13 11:01:27 +0000

    new pages.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import "./NavbarStyle.css"
+import "./Styles/NavbarStyle.css"
 import { MenuItems } from "./MenuItems";
 import { Link } from "react-router-dom";
 import logo from "/workspaces/codespaces-react/src/assets/logo-white.png"
