7dd16502c7f4dea340da28533ca037baf3e06a7e
rudrakshk-101 <83749855+rudrakshk-101@users.noreply.github.com>
2023-06-16 18:42:12 +0000

    blur heading commits

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import "/workspaces/codespaces-react/src/components/Styles/Home.css"
 import Navbar from '/workspaces/codespaces-react/src/components/Navbar.js';
-import home from "/workspaces/codespaces-react/src/assets/redforest3.png"
+import home from "/workspaces/codespaces-react/src/assets/backgroundimg1.jpg"
 import About from '/workspaces/codespaces-react/src/components/About.js';
 import Partners from '/workspaces/codespaces-react/src/components/Partners.js';
 import Speakers from "./Speakers";
@@ -10,11 +10,11 @@ return(
     <>
     <Navbar />
     <div className="hero"  id="hero">
-        <img alt="hero-img" src={home}/>
+        <img className="backgroundimgblur" alt="hero-img" src={home}/>
     </div>
     <div className="hero-text">
-        <h1>TEDx MIT UJJAIN</h1>
-        <p>15.7.2023</p>
+        <h1><span className="spanred">TEDx</span> MIT Ujjain</h1>
+        <p></p>
     </div>
     <About/>
     <Speakers/>
