da843b6f45d082724dcba4c41f22678d22c31e37
vinit gurjar <vinitfagna@gmail.com>
2023-06-17 04:05:07 +0000

    Speaker card for mobile view

--- src/components/Utility/SpeakerCard.js
+++ src/components/Utility/SpeakerCard.js
@@ -8,7 +8,7 @@ function SpeakerCard(props){
                 <img className="SpeakerCard-img" alt="SpeakerCard-poster" src={props.img}/>
                 <div className="SpeakerCard-text">
                 <h2 className="SpeakerCard-title">{props.title}</h2>
-                <div className="Speacker-text2">
+                <div className="Speacker-details">
                 <h4>TALK ON:-</h4>
                 <p className="SpeakerCard-description">{props.description}</p>
                 <a className="SpeakerCard-link" href={props.link}><i class='fa fa-twitter'></i></a>
